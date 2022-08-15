edd184ae40e26e97f5a3f527fb8fd9f818370e98
Ira <vishneviyshturman@gmail.com>
2022-08-15 16:43:48 +0300

    add tensor ring correction class

--- tens_correction.py
+++ tens_correction.py
@@ -114,8 +114,7 @@ class TKD_Factorizer(Tens_Factorizer):
     def factorize(self, K, rank, correct=False, args=None):
         self.K = K
         self.rank = rank
-        modes = (0,2)
-        self.core, self.factors = partial_tucker(self.K, modes, rank)
+        self.core, self.factors = partial_tucker(self.K, modes=(0,2), rank)
         if correct:
             self.delta = LA.norm(self.K - self.contract())
             self.correct(**args)
@@ -127,7 +126,6 @@ class TKD_Factorizer(Tens_Factorizer):
                 self.optimization_step_factors()
                 self.factors_shift()
             self.optimization_step_core()
-
 
     def optimization_step_core(self):
         L = self.find_L(core=True)
@@ -142,7 +140,7 @@ class TKD_Factorizer(Tens_Factorizer):
     def optimization_step_factors(self):
         B, C = self.factors
         L = self.find_L()
-        regressor = np.einsum('ijc,kc->ijk',self.core, C).reshape((self.rank[0], -1), order='C')
+        regressor = np.einsum('ijc,kc->ijk', self.core, C).reshape((self.rank[0], -1), order='C')
         regressor = LA.solve_triangular(L, regressor, lower=True)
         target = self.K.reshape((self.K.shape[0], -1), order='C')
         B_raw = Linreg_Optimizer().solve_matrix(regressor, target, self.delta)
@@ -174,4 +172,64 @@ class TKD_Factorizer(Tens_Factorizer):
         else:
             A = self.core.reshape((self.rank[0], -1))
             W = A @ A.T + np.eye(self.rank[0])
+        return LA.cholesky(W, lower=True)
+
+
+class TC_factorizer(Tens_Factorizer):
+    def __init__(self):
+        super().__init__()
+        self.rank = None
+        self.factors = None
+        self.n_factors = 3
+
+    def factorize(self, K, rank, correct=False, args=None):
+        self.K = K
+        self.rank = rank
+        self.factors = tensor_ring(K, rank).factors
+        if correct:
+            self.delta = LA.norm(self.K - self.contract())
+            self.correct(**args)
+        return self.factors
+
+    def contract(self):
+        A, B, C = self.factors
+        return np.einsum('kai,ibj,jck->abc', A, B, C)
+
+    def correct(self, n_iters):
+        for i in range(n_iters):
+            for j in range(self.n_factors):
+                self.optimization_step()
+                self.cyclic_shift()
+
+    def cyclic_shift(self):
+        self.K = self.K.transpose((2,0,1))
+        self.factors = [
+            self.factors[2],
+            self.factors[0],
+            self.factors[1]
+        ]
+        self.rank = [
+            self.rank[2],
+            self.rank[0],
+            self.rank[1],
+        ]
+
+    def optimization_step(self):
+        A, B, C = self.factors
+        r0, r1, r2 = self.rank
+        L = self.find_L()
+        regressor = np.einsum('ibj,jck->ikbc', B, C).reshape((r0 * r2, -1), order='C')
+        regressor = LA.solve_triangular(L, regressor, lower=True)
+        target = self.K.reshape((self.K.shape[0], -1), order='C')
+        A_raw = Linreg_Optimizer().solve_matrix(regressor, target, self.delta)
+        self.factors[0] = LA.solve_triangular(L.T, A_raw.T)
+        self.factors[0] = self.factors[0].reshape((r0, r2, -1), order='C')
+        self.factors[0] = self.factors[0].transpose((1,2,0))
+
+    def find_L(self, core=False):
+        A, B, C = self.factors
+        r0, r1, r2 = self.rank
+        B = B.reshape((r0, -1), order='C')
+        C = C.transpose(2,1,0).reshape((r2, -1), order='C')
+        W = LA.kron(B @ B.T, np.eye(r2)) + LA.kron(np.eye(r0), C @ C.T)
         return LA.cholesky(W, lower=True)
